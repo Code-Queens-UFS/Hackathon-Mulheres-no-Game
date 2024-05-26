97522aacafd2c9fe483ebc8f0f4e3e81b481bb94
Amanda de Jesus Melo <60000299+amandexspeed@users.noreply.github.com>
2024-05-25 22:38:33 -0300

    Tela inicial pronta

---
+++ gamersisters.client/src/App.jsx
@@ -0,0 +1,47 @@
+﻿import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="text">
+                    <h1>Gamer Sisters</h1>
+                    <p>Conectando as minas e as manas</p>
+                </div>
+
+                <div className="content">
+                    <img
+                        src="src/assets/imgs/logo.png"
+                        alt="Gamer Sister"
+                        className="girl-image"
+                    />
+                </div>
+            </div>
+
+            <div className="footer">
+
+                <div className="text">
+
+                    <p>O que deseja fazer por aqui?</p>
+
+                </div>
+                <div className="options">
+                    <button className="option">
+                        <span>Achar as manas</span>
+                        <span className="icon">🎮</span>
+                    </button>
+                </div>
+                <div className="options">
+                    <button className="option">
+                        <span>Ajudar as minas</span>
+                        <span className="icon">👑</span>
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default App;
